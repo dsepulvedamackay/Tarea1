d057b5dcb3ac146d6a290bf2b6b0b1715ebdf386
DSMackay <d.sepulveda.mackay@gmail.com>
2018-05-07 00:33:22 -0300

    TODO botones, alerta y vibracion

--- App.js
+++ App.js
@@ -1,23 +1,117 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert, ImageBackground, Image } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
 export default class App extends React.Component {
   render() {
+    return <RootStack />;
+  }
+}
+
+
+class LoginScreen extends React.Component {
+  static navigationOptions = {
+    header: null
+    
+  };
+
+  exit = () => {
+    Alert.alert(
+      'Salir',
+      '¿Seguro de quieres salir?',
+      [
+        {text: 'No', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: 'Si', onPress: () => this.props.navigation.goBack() },
+      ],
+      { cancelable: false }
+    )
+  }
+
+  render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+       <ImageBackground
+        style={ styles.container}
+        source={require("./img/backgound.png")}>
+
+        <Image source={require("./img/Original-Logo.png")}
+           style={styles.logo}
+          />
+
+          <TextInput
+            style={styles.textInput}
+            autoCapitalize="none"
+            placeholder="Usuario"
+          />
+          <TextInput
+            secureTextEntry
+            style={styles.textInput}
+            autoCapitalize="none"
+            placeholder="Contraseña"
+          />
+          <Button title="Entrar" 
+            onPress={() => this.props.navigation.navigate('Home')}>
+          </Button>
+
+          <Button title="Salir" 
+            onPress={() => this.exit()}>
+          </Button>
+
+        </ImageBackground>
+
+     
+    );
+  }
+}
+
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
+  };
+  
+  render() {
+    return (
+      <View>
       </View>
     );
   }
 }
 
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Login: {
+      screen: LoginScreen,
+    },
+  },
+  {
+    initialRouteName: 'Login',
+  }
+);
+
+
+
 const styles = StyleSheet.create({
   container: {
+    width: undefined,
+    height: undefined,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  textInput: {
+    paddingRight: 10,
+    paddingLeft: 10,
+    height: '7%',
+    width: '70%',
+    marginTop: '5%',
+    backgroundColor: 'white'
+  },
+  logo: {
+    width: '70%',
+    height: '30%',
+  },
+  
 });
